71c94fee0ce0f02b1adebd4502a63c8c480527f6
Jerry Arhawho <jerryarhawho@gmail.com>
2023-12-21 22:57:18 +0000

    Customer, DB and app properties

---
+++ src/main/java/com/jayklef/JerryBankProject/model/Customer.java
@@ -0,0 +1,41 @@
+package com.jayklef.JerryBankProject.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String middleName;
+    private String email;
+    private String gender;
+    private String telephone;
+    private String alternateNumber;
+    private String address;
+    private String state;
+    private String status;
+    private String accountNumber;
+    private BigDecimal accountBalance;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
+}
