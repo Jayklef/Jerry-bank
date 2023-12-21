240017be503b7f77ed4f24b7c8574515730bf7c7
Jerry Arhawho <jerryarhawho@gmail.com>
2023-12-21 22:09:25 +0000

    initial project setup

---
+++ src/main/java/com/jayklef/JerryBankProject/JerryBankProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jayklef.JerryBankProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerryBankProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerryBankProjectApplication.class, args);
+	}
+
+}
