b4ae529bc1101a1003415a547ef7db8e91048003
Jerry Arhawho <jerryarhawho@gmail.com>
2023-12-22 13:45:29 +0000

    CreateCustomer controller

---
+++ src/main/java/com/jayklef/JerryBankProject/controller/CustomerController.java
@@ -0,0 +1,24 @@
+package com.jayklef.JerryBankProject.controller;
+
+import com.jayklef.JerryBankProject.dto.BankResponse;
+import com.jayklef.JerryBankProject.dto.CustomerRequest;
+import com.jayklef.JerryBankProject.model.Customer;
+import com.jayklef.JerryBankProject.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/customer/")
+public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @PostMapping("create")
+    public BankResponse createCustomer(@RequestBody CustomerRequest customerRequest){
+        return customerService.createAccount(customerRequest);
+    }
+}
